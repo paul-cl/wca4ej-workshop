df5f0528c918f1b743cd83e7f5634770b66eec81
cloud application <paulforever012@gmail.com>
2025-02-18 13:21:01 +0900

    add test

--- modresorts-twas-j8/src/test/java/com/acme/modres/MyClassTest.java
+++ modresorts-twas-j8/src/test/java/com/acme/modres/MyClassTest.java
@@ -1,10 +1,22 @@
 package com.acme.modres;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyClassTest {
+
     @Test
-    void testMethod() {
-        // 테스트 코드
+    public void testWelcomeMessage() throws Exception {
+        WelcomeServlet servlet = new WelcomeServlet();
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        
+        servlet.doGet(request, response);
+        
+        String content = response.getContentAsString();
+        assertTrue(content.contains(" Enjoy!"), "응답에 'Enjoy!' 메시지가 포함되어야 합니다");
     }
 }
